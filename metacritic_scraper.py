39d700127690f3da2e46b73e3ae25b429a825124
yairVag <yairvagshal@gmail.com>
2022-02-24 15:41:40 +0200

    combine both functions in metacritic_scraper.py

--- metacritic_scraper.py
+++ metacritic_scraper.py
@@ -13,8 +13,8 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
-url = "https://www.metacritic.com/browse/albums/score/metascore/all?sort=desc"
+URL = "https://www.metacritic.com/browse/albums/score/metascore/all?sort=desc"
+SITE_ADDRESS = "https://www.metacritic.com"
 
 
 def scrape(page_url):
@@ -58,30 +58,72 @@ def scrape(page_url):
     summaries = [i.get_text().lstrip(str_to_strip_from_beg).rstrip(str_to_strip_from_end) for i in summary_text]
 
     # Links
+<<<<<<< HEAD
     site_name = "https://metacritic.com"
     links = [(site_name + i["href"]) for i in album_name_text]
+=======
+    links = [(SITE_ADDRESS + i["href"]) for i in album_name_text]
+>>>>>>> 31ca876 (combine both functions in metacritic_scraper.py)
 
+    summery = ({"Album": album_names,
+                "Artist": artist_names,
+                "Metascore": metascores,
+                "User Score": userscores,
+                "Release Date": release_dates,
+                "Summary": summaries,
+                "Link to Album Page": links})
 
+    summery.update(scrape_album_page(links))
 
-
-
-    # Scraping each album page - Yair's code
-    # Iterate over every element in links
-
-
-
-
-
-    table_dict = ({"Album": album_names,
-                   "Artist": artist_names,
-                   "Metascore": metascores,
-                   "User Score": userscores,
-                   "Release Date": release_dates,
-                   "Summary": summaries,
-                   "Link to Album Page": links})
-    top_albums = pd.DataFrame(table_dict)
+    top_albums = pd.DataFrame(summery)
     top_albums.to_csv("top albums.csv")
     print(top_albums)
 
 
-scrape(url)
+def scrape_album_page(pages_url):
+    result = {}
+    for url in pages_url:
+
+        # Getting page information
+        source = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}).text
+        soup = BeautifulSoup(source, 'html.parser')
+
+        result.setdefault('artist_link', []).append(SITE_ADDRESS + soup.find('div', class_='product_artist').a['href'])
+
+        publisher_html = soup.find('span', class_='data', itemprop='publisher')
+        result.setdefault('publisher_link', []).append([SITE_ADDRESS + publisher_html.a['href']])
+        result.setdefault('publisher_name', []).append(publisher_html.a.span.text.strip())
+
+        result.setdefault('album_cover_image', []).append(publisher_html.a.span.text.strip())
+
+        genres = soup.find('li', class_='summary_detail product_genre')
+        result.setdefault('genres_list', []).append('\n'.join([genre.text for genre in genres.findAll('span')]))
+
+        result.setdefault('meta_score_count', []).append(soup.find('span', itemprop="reviewCount").text.strip())
+        result.setdefault('meta_score_link', []).append(
+            SITE_ADDRESS + soup.find('li', class_="nav nav_critic_reviews").span.span.a["href"])
+
+        result.setdefault('user_score_link', []).append(
+            SITE_ADDRESS + soup.find('li', class_='nav nav_user_reviews').span.span.a["href"])
+
+        try:
+            user_score_html = soup.find('div', class_="userscore_wrap feature_userscore")
+            result.setdefault('user_score_count', []).append(user_score_html.find('span', class_='count').a.text)
+        except Exception:
+            result.setdefault('user_score_count', []).append('')
+
+        result.setdefault('details_credits_link', []).append(
+            SITE_ADDRESS + soup.find('li', class_="nav nav_details last_nav").span.span.a["href"])
+
+        # Link to Buy the Album
+        buy_album_link = soup.find('td', class_="esite_img_wrapper")
+        if buy_album_link:
+            result.setdefault('buy_album_link', []).append(buy_album_link.a["href"])
+        else:
+            result.setdefault('buy_album_link', []).append('')
+
+    return result
+
+
+if __name__ == '__main__':
+    scrape(URL)
